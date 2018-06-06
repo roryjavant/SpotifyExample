806edf667c28343e51014a9234fdf1d19732a7b2
Rory Avant <rory.avant@selu.edu>
2018-06-06 11:05:17 -0500

    settings page, home page

---
+++ SpotifyExample/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  SpotifyExample
+//
+//  Created by Rory Avant on 6/6/18.
+//  Copyright © 2018 Rory Avant. All rights reserved.
+//
+
+import Foundation
---
+++ SpotifyExample/Settings/Settings.swift
@@ -0,0 +1,9 @@
+//
+//  Settings.swift
+//  SpotifyExample
+//
+//  Created by Rory Avant on 6/6/18.
+//  Copyright © 2018 Rory Avant. All rights reserved.
+//
+
+import Foundation
