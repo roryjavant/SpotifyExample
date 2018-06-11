ed54dec4133171dd1d1d852723645bbfc337660b
Rory Avant <rory.avant@selu.edu>
2018-06-11 14:47:02 -0500

    save

---
+++ SpotifyExample/Controls/ChainsStackView.swift
@@ -0,0 +1,21 @@
+//
+//  ChainsStackView.swift
+//  SpotifyExample
+//
+//  Created by Rory Avant on 6/11/18.
+//  Copyright © 2018 Rory Avant. All rights reserved.
+//
+
+import UIKit
+
+class ChainsStackView: UIStackView {
+
+    private var chainActivated : Bool
+    
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
+}
