395d710285b0c788c548a278e2e96e40f38812a5
Rory Avant <rory.avant@selu.edu>
2018-06-13 15:00:14 -0500

    Settings back button working

---
+++ SpotifyExample/Settings/SettingsTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SettingsTableViewController.swift
+//  SpotifyExample
+//
+//  Created by Rory Avant on 6/13/18.
+//  Copyright © 2018 Rory Avant. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+
+    @IBOutlet weak var backButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: false)
+    }
+    
+
+}
---
+++ SpotifyExample/Settings/SettingsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SettingsViewController.swift
+//  SpotifyExample
+//
+//  Created by Rory Avant on 6/13/18.
+//  Copyright © 2018 Rory Avant. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
